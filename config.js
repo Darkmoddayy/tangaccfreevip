0238bdd900bca2912b4790a271961434f31ebe67
Darkmoddayy <giamlagroblox@gmail.com>
2026-02-18 17:15:58 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyCNeMxQYCA3C58k1RtC4XyKayQnk-xYxkg",
+authDomain: "tangaccfreevip-2f58c.firebaseapp.com",
+databaseURL: "https://tangaccfreevip-2f58c-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "tangaccfreevip-2f58c",
+storageBucket: "tangaccfreevip-2f58c.appspot.com",
+messagingSenderId: "970018428565",
+appId: "1:970018428565:web:3d8b15dc15b29977386d9c"
+};
